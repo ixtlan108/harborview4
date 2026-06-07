80e63d7286181aa0d8e66d706482f7a4f9e3057e
Ixtlan <ixtlan@gmail.com>
2026-06-07 08:38:31 +0200

    WIP

--- src/integration/java/harborview/stockmarket/StockMarketIntegrationTest.java
+++ src/integration/java/harborview/stockmarket/StockMarketIntegrationTest.java
@@ -7,6 +7,8 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.modulith.test.ApplicationModuleTest;
 
+import java.time.LocalDate;
+
 import static org.springframework.modulith.test.ApplicationModuleTest.BootstrapMode.ALL_DEPENDENCIES;
 
 
@@ -23,5 +25,22 @@ public class StockMarketIntegrationTest {
     void test_fetch_critters() {
         var critters = core.getCritters(11);
         Assertions.assertEquals(1,critters.size());
+
+        var purchase = critters.getFirst();
+
+        Assertions.assertEquals("YAR7C800",purchase.getOptionName());
+        Assertions.assertEquals("c", purchase.getOptionType());
+        Assertions.assertEquals(2, purchase.getOptionId());
+        //Assertions.assertEquals(2, purchase.getStockOptionBuy());
+
+        Assertions.assertEquals("YAR",purchase.getTicker());
+        Assertions.assertEquals(11,purchase.getPurchaseType());
+        Assertions.assertEquals(LocalDate.of(2027,3,19),purchase.getExpiry());
+        Assertions.assertEquals(LocalDate.of(2026,6,3),purchase.getLocalDx());
+        Assertions.assertEquals(312.0, purchase.getPrice(), 0.01);
+        Assertions.assertEquals(516.0, purchase.getSpotAtPurchase(), 0.01);
+        Assertions.assertEquals(300.0, purchase.getBuyAtPurchase(), 0.01);
+        Assertions.assertEquals(10, purchase.getVolume());
+
     }
 }
